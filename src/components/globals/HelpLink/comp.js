3355841e3ac8493864c72a42a4cd0cd90fefb9d1
Yannick Spreen <yannick.spreen@gmail.com>
2019-12-03 14:41:18 -0500

    Set help links.

--- src/components/globals/HelpLink/comp.js
+++ src/components/globals/HelpLink/comp.js
@@ -4,6 +4,37 @@ export default {
       $event.preventDefault();
       var win = window.open("#/docs/" + this.page, "_blank");
       win.focus();
+    },
+    setPath(p) {
+      p = p.replace("#", ":");
+      let main = p.split(":")[0];
+      if (!main.endsWith("/")) {
+        main += "/";
+      }
+      if (main.startsWith("/")) {
+        main = main.slice(1);
+      }
+      if (!p.includes(":")) {
+        return (this.path = main);
+      }
+      let sub = p.split(":")[1];
+      if (sub.endsWith("/")) {
+        sub = sub.slice(0, -1);
+      }
+      return (this.path = `${main}:${sub}`);
+    }
+  },
+  data() {
+    return {
+      path: ""
+    };
+  },
+  mounted() {
+    this.setPath(this.page);
+  },
+  watch: {
+    page(v) {
+      this.setPath(v);
     }
   },
   props: ["page", "sm", "pad"]
